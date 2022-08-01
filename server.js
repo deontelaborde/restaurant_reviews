53720d52a0d4cba13dc26e39b720d96004e25eb3
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-01 10:04:44 -0400

    added nodemon and changed root routing to /restaurants

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+//middleware
+app.use(express.static(`${__dirname}/client/build`))
+
 // Test route
-app.get('/', (req, res) => {
+app.get('/restaurants', (req, res) => {
   res.send('I have hit the root route')
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, () => {
