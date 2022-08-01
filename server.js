407f6e1d8dc8e65f204c4dc5d0a390a87920213f
Deonte LaBorde <deonte.laborde@gmail.com>
2022-08-01 10:44:33 -0400

    create route to server

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
+const db = require('./db')
 
 const app = express()
+
+const { Restaurant } = require('./models')
 
 //middleware
 app.use(express.static(`${__dirname}/client/build`))
 
 // Test route
 app.get('/restaurants', (req, res) => {
+  let restaurants = await async 
   res.send('I have hit the root route')
 })
 
